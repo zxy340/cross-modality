fef1097966cd0ad9916be85d097657d7da08383c
zxy340 <zhang376@buffalo.edu>
2022-08-07 00:18:27 -0400

    modify realtime

--- detect_realtime.py
+++ detect_realtime.py
@@ -17,6 +17,9 @@ import os
 import sys
 from pathlib import Path
 import numpy as np
+import multiprocessing
+import time
+
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
@@ -60,75 +63,73 @@ def run(weights=ROOT / './runs/train/new_Kinect_yolov3-tiny/weights/best.pt',  #
     stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
-    # Dataloader
-    typ = np.dtype(np.uint16, (414, 512))
-    Depth_data = np.fromfile(source, dtype=typ)
-    max_value = Depth_data.max()
-    Depth_data = Depth_data / max_value * 255
-    name = './realtime/kinect.jpg'
-    cv2.imwrite(name, Depth_data)
-    dataset = LoadImages(name, img_size=imgsz, stride=stride, auto=pt and not jit)
-
-    # Run inference
-    if pt and device.type != 'cpu':
-        model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
-    dt, seen = [0.0, 0.0, 0.0], 0
-    count, total = 0, 0
-    for path, im, im0s, vid_cap, s in dataset:
+    while(1):
         t1 = time_sync()
-        im = torch.from_numpy(im).to(device)
-        im = im.half() if half else im.float()  # uint8 to fp16/32
-        im /= 255  # 0 - 255 to 0.0 - 1.0
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
+        # Dataloader
+        typ = np.dtype((np.uint16, (424, 512)))
+        Depth_data = np.fromfile(source, dtype=typ)
+        Depth_data = Depth_data.squeeze()
+        max_value = Depth_data.max()
+        Depth_data = Depth_data / max_value * 255
+        Depth_data = cv2.resize(Depth_data, (416, 416))
+        name = './realtime/kinect.jpg'
+        cv2.imwrite(name, Depth_data)
+        dataset = LoadImages(name, img_size=imgsz, stride=stride, auto=pt and not jit)
+
+        # Run inference
+        if pt and device.type != 'cpu':
+            model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+        seen, count =0, 0
+        for path, im, im0s, vid_cap, s in dataset:
+            im = torch.from_numpy(im).to(device)
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+            # Inference
+            pred = model(im, augment=augment, visualize=visualize)
+
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            pred[0] = pred[0][torch.arange(pred[0].size())==0]
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+                p = Path(p)  # to Path
+                s += '%gx%g ' % im.shape[2:]  # print string
+                annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+                if len(det):
+                    count = count + 1  # record the number of detected images
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+
+                # Stream results
+                im0 = annotator.result()
+                old_mtime = time.ctime(os.path.getmtime(source))
+                while(1):
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(10)  # 1 millisecond
+                    if time.ctime(os.path.getmtime(source)) != old_mtime:
+                        break
         t2 = time_sync()
-        dt[0] += t2 - t1
-
-        # Inference
-        pred = model(im, augment=augment, visualize=visualize)
-        t3 = time_sync()
-        dt[1] += t3 - t2
-
-        # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        dt[2] += time_sync() - t3
-
-        # Process predictions
-        for i, det in enumerate(pred):  # per image
-            seen += 1
-            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
-
-            p = Path(p)  # to Path
-            s += '%gx%g ' % im.shape[2:]  # print string
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-            if len(det):
-                count = count + 1  # record the number of detected images
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    c = int(cls)  # integer class
-                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                    annotator.box_label(xyxy, label, color=colors(c, True))
-
-            # Print time (inference-only)
-            LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
-
-            # Stream results
-            im0 = annotator.result()
-            cv2.imshow(str(p), im0)
-            cv2.waitKey(1)  # 1 millisecond
+        LOGGER.info(f'Done. ({t2 - t1:.3f}s)')
 
 def main():
-    check_requirements(exclude=('tensorboard', 'thop'))
     run()
-
 
 if __name__ == "__main__":
     main()
